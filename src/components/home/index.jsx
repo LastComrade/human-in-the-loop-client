a30bf8deff1584da866a1fe164ba7a67e0ccbb05
Konark Lohat <konarklohat123456@gmail.com>
2022-08-13 18:13:53 +0530

    Homepage screen bug fixed

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -9,7 +9,7 @@ import TextCard from "../text/TextCard";
 
 const Home = () => {
   return (
-    <div className="bg-black pt-10 overflow-hidden relative z-10">
+    <div className="bg-black min-h-screen pt-10 overflow-hidden relative z-10">
       <Header />
       <section className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-6">
